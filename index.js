8102e9e6c0276cd32c4129a4828fe0149436e722
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2025-02-11 11:32:34 +0700

    edit note api and get all notes api

--- index.js
+++ index.js
@@ -144,6 +144,51 @@ app.post("/add-note", authenticateToken, async (req, res) => {
     };
 });
 
+// Edit Note
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const { user } = req.user;
+
+    if (!title && !content && !tags) {
+        return res.status(400).json({ error: true, message: "No changes provided" });
+    }
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+        if (!note) {
+            return res.status(404).json({ error: true, message: "Note Not Found" });
+        }
+
+        if (title) note.title = title;
+        if (content) note.content = content;
+        if (tags) note.tags = tags;
+        if (isPinned) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({ error: false, note, message: "Note Updated Successfully" });
+    } catch (error) {
+        return res.status(500).json({ error: true, message: "Interval Server Error" });
+    }
+});
+
+// Get All Notes
+app.get("/get-all-notes", authenticateToken, async (req, res) => {
+    const { user } = req.user;
+
+    try {
+        const notes = await Note.find({ userId: user._id
+        }).sort({ isPinned: -1 });
+
+        return res.json({ error: false, notes, message: "All Notes Retrieved Successfully" });
+        
+    } catch (error) {
+        return res.status(500).json({ error: true, message: "Interval Server Error" });
+    }
+});
+
 app.listen(8000);
 
 module.exports = app;
