65361de1b7ad8110657ef58a15cfd17a42fcf859
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2025-02-11 10:37:14 +0700

    new note api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 mongoose.connect(config.connectionsString);
 
 const User = require("./models/user.model");
+const Note = require("./models/note.model");
 
 const express = require('express');
 const cors = require('cors');
@@ -112,7 +113,36 @@ app.post("/login", async (req, res) => {
             message: "Invalid Credentials",
         });
     }
-})
+});
+
+// Add Note
+app.post("/add-note", authenticateToken, async (req, res) => {
+    const { title, content, tags } = req.body;
+    const { user } = req.user;
+
+    if (!title) {
+        return res.status(400).json({ error: true, message: "Title is required" });
+    }
+
+    if (!content) {
+        return res.status(400).json({ error: true, message: "Content is required" });
+    }
+
+    try {
+        const note = new Note({
+            title,
+            content,
+            tags: tags || [],
+            userId: user._id,
+        });
+
+        await note.save();
+
+        return res.json({ error: false, note, message: "Note Added Successfully" });
+    } catch (error) {
+        return res.status(500).json({ error: true, message: "Interval Server Error" });
+    };
+});
 
 app.listen(8000);
 
