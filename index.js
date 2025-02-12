3f8bf76551cf7f08c33799c272837ad0960e62e9
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2025-02-12 11:59:28 +0700

    search note api

--- index.js
+++ index.js
@@ -254,6 +254,30 @@ app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
     }
 });
 
+// Search Note
+app.get("/search-note/", authenticateToken, async (req, res) => {
+    const { user } = req.user;
+    const { query } = req.query;
+
+    if (!query) {
+        return res.status(400).json({ error: true, message: "Search query is required" });
+    }
+
+    try {
+        const matchingNotes = await Note.find({
+            userId: user._id,
+            $or: [
+                { title: { $regex: new RegExp(query, "i")}},
+                { content: { $regex: new RegExp(query, "i")}},
+            ],
+        });
+
+        return res.json({ error: false, notes: matchingNotes, message: "Notes matching the search query retrieved successfully",})
+    } catch (error) {
+        return res.status(500).json({ error: true, message: "Interval Server Error",});
+    }
+});
+
 app.listen(8000);
 
 module.exports = app;
